292fdaf1dfbd863dff02961c9a5155cd375b265a
Arthur Guiot <arguiot@gmail.com>
2017-05-26 13:17:06 +1000

    Updated

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,15 @@
-$(window).scroll(function() {
-  var wScroll = $(this).scrollTop();
-  var jumpIn  = $('header').height();
-  var jumpOut = $('#docs-cta').offset().top - wScroll - $(this).height() + 200;
-  if (wScroll >= jumpIn && jumpOut > 0) {
-    $('.gist.js-activated').addClass('visible')
-  } else {
-    $('.gist.js-activated').removeClass('visible')
-  }
-});
+// $(window).scroll(function() {
+//   var wScroll = $(this).scrollTop();
+//   var jumpIn  = $('header').height();
+//   var jumpOut = $('#docs-cta').offset().top - wScroll - $(this).height() + 200;
+//   if (wScroll >= jumpIn && jumpOut > 0) {
+//     $('.gist.js-activated').addClass('visible')
+//   } else {
+//     $('.gist.js-activated').removeClass('visible')
+//   }
+// });
 
-$('nav .hamburger').click(function () {
+$('nav .hamburger').click(() => {
   $('.js-toggled').toggleClass('visible');
   if ($(".js-toggled").hasClass("visible")) {
 		$("body").scrollDisable();
@@ -17,20 +17,20 @@ $('nav .hamburger').click(function () {
 });
 
 $(window).scroll(function () {
-	var wScroll = $(this).scrollTop();
-	  var jumpIn  = $('header').height() + 100;
+	const wScroll = $(this).scrollTop();
+	  const jumpIn  = $('header').height() + 100;
 	  if (wScroll > jumpIn) {
 	    $('footer').show();
 	  } else {
 	    $('footer').hide();
 	  }
 	$("header").css({
-		'top': 0-($(this).scrollTop() / 3) + "px"
+		'top': `${0-($(this).scrollTop() / 3)}px`
 	});
 });
-$(document).ready(function () {
-	$.getJSON("https://unpkg.com/noobscroll?json", function (data) {
-		var size = data.size / 1024;
+$(document).ready(() => {
+	$.getJSON("https://unpkg.com/noobscroll?json", data => {
+		let size = data.size / 1024;
 		size = Math.round(size * 100) / 100
 		$(".kb").text(size);
 	});
