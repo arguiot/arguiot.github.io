d30b8b38b191b2e1229eba02a46fa6a0ba1574fe
Arthur Guiot <arguiot@gmail.com>
2018-03-04 19:59:38 +0800

    Fixed footer

--- js/functions.js
+++ js/functions.js
@@ -44,9 +44,25 @@ class Parralax {
         this.els.push(...arguments)
     }
     _addEventListeners() {
+		let footer = document.querySelector("footer")
+		let menuicons = document.querySelectorAll(".menu-icon > *")
         window.addEventListener("scroll", () => {
             window.requestAnimationFrame(() => {
                 this.render(window.scrollY);
+
+				if (window.scrollY >= window.innerHeight * 3) {
+					footer.style.position = "fixed";
+					footer.style.top = "0"
+					for (let i of menuicons) {
+						i.style.background = "#fff"
+					}
+				} else {
+					footer.style.position = "absolute";
+					footer.style["top"] = "300vh"
+					for (let i of menuicons) {
+						i.style.background = "#000"
+					}
+				}
             })
         })
     }
@@ -71,7 +87,9 @@ class Parralax {
             const percent = diff / wH;
             const f = this.f[i]
 			const m = this.m[i]
-            this.els[i].style.transform = `translate3d(0, ${percent * 1000 * f + (wH / 100 * m)}px, 0)`
+			const pixel = percent * 1000 * f + (wH / 100 * m)
+            this.els[i].style.transform = `translate3d(0, ${pixel}px, 0)`
+			// this.els[i].style['margin-top'] = `${pixel / 2}px`
         }
     }
 }
