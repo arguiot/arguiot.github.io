64753c9f40171351236f3c9201af89ebad7b0d79
Arthur Guiot <arguiot@gmail.com>
2020-10-06 09:55:21 -0400

    Ready to publish

--- js/entry.js
+++ js/entry.js
@@ -8,7 +8,18 @@ class Main {
 	setupUI() {
 		if (matchMedia) {
 			const mq = window.matchMedia("(max-width: 600px)")
-			mq.addListener(this.widthChange.bind(this))
+			const handler = this.widthChange.bind(this)
+			try {
+				// Chrome & Firefox
+				mq.addEventListener('change', handler);
+			} catch (e1) {
+				try {
+					// Safari
+					mq.addListener(handler);
+				} catch (e2) {
+					console.error(e2);
+				}
+			}
 			this.widthChange(mq)
 		} else {
 			this.largeWindow()
